004c401bf1643e777157e6ebcc5b41ad36b76aff
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2025-08-20 23:00:43 +0200

    getting started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function LandingPage() {
 	return (
 		<main className="hero h-full">
@@ -5,6 +7,11 @@ export default function LandingPage() {
 				<div className="max-w-md flex flex-col gap-6">
 					<h1 className="text-5xl font-bold">Steam Bookmarks</h1>
 					<p>Home page for the Steam overlay browser</p>
+					<div>
+						<Link className="link" href="/getting-started">
+							Getting Started
+						</Link>
+					</div>
 				</div>
 			</div>
 		</main>
