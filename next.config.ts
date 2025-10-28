fceb49cdf60033d9cdba1e0c6a068b41e166f6f9
Tanguy Michardière <54041658+tanguyMichardiere@users.noreply.github.com>
2025-10-28 17:40:45 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,13 @@
 import withNextBundleAnalyzer from "@next/bundle-analyzer";
+import type { NextConfig } from "next";
 import { headers } from "./headers.config.mjs";
 
-/** @type {import("next").NextConfig} */
-let nextConfig = {
+let nextConfig: NextConfig = {
 	// type checking is done in CI
 	typescript: { ignoreBuildErrors: true },
 	typedRoutes: true,
 	reactStrictMode: true,
 	reactCompiler: true,
-	experimental: {
-		turbopackFileSystemCacheForDev: true,
-	},
 	headers() {
 		return Promise.resolve([{ headers, source: "/:path*" }]);
 	},
